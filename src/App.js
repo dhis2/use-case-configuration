fc7819af648fa40bb9c7ba29510eac9508d396c8
Milagros <sharmynrodriguez@gmail.com>
2022-12-18 00:42:11 +0100

    feat: basic app with auth wall and basic user info

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
-import React from 'react'
-import { DataQuery } from '@dhis2/app-runtime'
 import i18n from '@dhis2/d2-i18n'
+import { CssVariables, CssReset } from '@dhis2/ui'
+import React from 'react'
+import { useAppContext } from './app-context'
+import { AppProvider } from './app-context/app-provider'
 import classes from './App.module.css'
+import { AuthWall } from './auth'
+import { useIsAuthorized } from './auth/use-is-authorized'
 
-const query = {
-    me: {
-        resource: 'me',
-    },
-}
+const App = () => (
+    <>
+        <CssReset />
+        <CssVariables spacers colors />
 
-const MyApp = () => (
-    <div className={classes.container}>
-        <DataQuery query={query}>
-            {({ error, loading, data }) => {
-                if (error) return <span>ERROR</span>
-                if (loading) return <span>...</span>
-                return (
-                    <>
-                        <h1>
-                            {i18n.t('Hello {{name}}', { name: data.me.name })}
-                        </h1>
-                        <h3>{i18n.t('Welcome to DHIS2!')}</h3>
-                    </>
-                )
-            }}
-        </DataQuery>
-    </div>
+        <AppProvider>
+            <AuthWall>
+                <div className={classes.container}>
+                    <BasicData />
+                </div>
+            </AuthWall>
+        </AppProvider>
+    </>
 )
 
-export default MyApp
+export default App
+
+// TODO: this is temporary app that displays user info, should change to the real app
+const BasicData = () => {
+    const { authorities, username } = useAppContext()
+    const { hasAccess } = useIsAuthorized()
+    const dataStore = hasAccess ? 'have' : 'do not have'
+
+    return (
+        <div>
+            <h1>{i18n.t('Hello {{name}}', { name: username })}</h1>
+            <h3>{i18n.t('Welcome to DHIS2!')}</h3>
+            <p>
+                {i18n.t(
+                    'currently you have {{authorities}} authorities and {{dataStore}} access to datastore USE_CASES name space',
+                    { authorities, dataStore }
+                )}
+            </p>
+        </div>
+    )
+}
