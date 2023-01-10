38319f46c8dbd53775afdaf3c64e57aedb65d2cf
Milagros Rodríguez <sharmynrodriguez@gmail.com>
2023-01-10 09:16:53 +0100

    feat: [DHIS2-14354] basic app with auth wall (#2)
    * feat: app context
    * feat: authority wall
    * feat: basic first launch dialog
    * feat: basic app with auth wall and basic user info

---
+++ src/auth/use-is-authorized.js
@@ -0,0 +1,13 @@
+import { useAppContext } from '../app-context'
+import { NAMESPACE } from '../shared'
+
+const ALL = 'ALL'
+export const useIsAuthorized = () => {
+    //TODO: define authority name reserved for this app other than ALL
+
+    const { authorities, dataStore } = useAppContext()
+    const hasAppAccess = authorities.some((authority) => authority === ALL)
+    const hasNamespace = dataStore.some((key) => key === NAMESPACE)
+
+    return { hasAppAccess, hasNamespace }
+}
---
+++ src/shared/dataStore.js
@@ -0,0 +1,1 @@
+export const NAMESPACE = 'USE_CASES'
