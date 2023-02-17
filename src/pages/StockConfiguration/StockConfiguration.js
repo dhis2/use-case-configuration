3504ec3cbe54eac594b8e1c3aa834856513cd54b
Milagros <sharmynrodriguez@gmail.com>
2023-02-17 22:59:43 +0100

    feat: create Configure Program Section

--- src/pages/StockConfiguration/StockConfiguration.js
+++ src/pages/StockConfiguration/StockConfiguration.js
@@ -1,30 +1,65 @@
 import i18n from '@dhis2/d2-i18n'
-import React from 'react'
-import { useAppContext } from '../../app-context'
-import { useIsAuthorized } from '../../auth/use-is-authorized'
+import isEqual from 'lodash/isEqual'
+import isNil from 'lodash/isNil'
+import React, { useState, useEffect } from 'react'
 import { Page } from '../../components'
+import { useGetDataStore, useUpdateUseCases } from '../../hooks'
+import AddConfiguration from './AddConfiguration'
 
 const TITLE = i18n.t('Configure Program')
 const DESCRIPTION = i18n.t('Configure Program according to a specific type')
 
-// TODO: this is temporary page that displays user info, should be changed to the stock use case
 export const StockConfiguration = () => {
-    const { authorities, username } = useAppContext()
-    const { hasAccess } = useIsAuthorized()
-    const dataStore = hasAccess ? 'have' : 'do not have'
+    const { stockUseCases, loading } = useGetDataStore()
+    const { mutate, error, data } = useUpdateUseCases()
+    const [initialUseCasesList, setInitialList] = useState([])
+    const [useCasesList, setCurrentList] = useState([])
+    const [disable, setDisable] = useState(true)
+
+    /* Populate use case list based on datastore  */
+    useEffect(() => {
+        if (!isNil(stockUseCases)) {
+            const dataStoreToUseCaseList = []
+            stockUseCases.forEach((config) =>
+                dataStoreToUseCaseList.push(config)
+            )
+            setInitialList(dataStoreToUseCaseList)
+            setCurrentList(dataStoreToUseCaseList)
+        }
+    }, [stockUseCases])
+
+    /* Enable save button if the use cases list changes */
+    useEffect(() => {
+        if (!isNil(useCasesList)) {
+            !isEqual(useCasesList, initialUseCasesList)
+                ? setDisable(false)
+                : setDisable(true)
+        }
+    }, [useCasesList])
+
+    const saveData = async () => {
+        await mutate({ settings: stockUseCases })
+    }
 
     return (
-        <Page title={TITLE} desc={DESCRIPTION} dataTest="stock">
-            <>
-                <h1>{i18n.t('Hello {{name}}', { name: username })}</h1>
-                <h3>{i18n.t('Welcome to DHIS2!')}</h3>
-                <p>
-                    {i18n.t(
-                        'currently you have {{authorities}} authorities and {{dataStore}} access to datastore USE_CASES name space',
-                        { authorities, dataStore }
-                    )}
-                </p>
-            </>
+        <Page
+            title={TITLE}
+            desc={DESCRIPTION}
+            dataTest="stock"
+            loading={loading}
+            handleSave={saveData}
+            disabled={disable}
+            error={error}
+            success={data}
+        >
+            <div>
+                <AddConfiguration
+                    useCases={useCasesList}
+                    handleUseCases={setCurrentList}
+                />
+
+                <div> summary Table </div>
+            </div>
         </Page>
     )
 }
