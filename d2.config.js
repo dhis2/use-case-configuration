d7578e389147fa8ea3373dbb8efb95b78e45c8c1
Milagros <sharmynrodriguez@gmail.com>
2023-05-25 17:40:41 +0200

    fix: author and description app

--- d2.config.js
+++ d2.config.js
@@ -1,5 +1,6 @@
 const config = {
     title: 'Use Case Configuration',
+    description: 'Connect components of use cases to the Android Capture app.',
     type: 'app',
     entryPoints: {
         app: './src/App.js',
